2d510da676e3f62150f22494c59c73ed9ed28812
Zac Allen <zac.allen@outlook.com>
2021-09-27 18:46:14 +0200

    added getWeather() method and input field for user to select place, moved the post call to that method

--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -3,7 +3,12 @@
         {{welcome}}
     </h1>
     <div>
+        <form action="" method="get"></form>
+        <p>To get started, type in the region, town or city you'd like to see the weather: <input type="text" v-model="location"></p>
+        <button @click="getWeather()">Get Weather</button>
+        <p>
         {{data}}
+        </p>
     </div>
 </template>
 
@@ -23,16 +28,21 @@ export default {
           apiMethod: '/current.json',
           key: 'key='+process.env.VUE_APP_API_KEY,
           data: null,
-          location: 'Dublin',
+          location: '',
           aqi: 'yes'
       }
   },
   mounted() {
+
+  },
+  methods: {
+      getWeather(){
       axios.post(this.baseUrl+this.apiMethod+'?'+this.key+'&'+'q='+this.location+'&'+'aqi='+this.aqi)
         .then(response => {
             this.data = response.data
             console.log(this.data)
         })
+      }
   }
 }
 </script>
